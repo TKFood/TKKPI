6894d707c7d3580a35e7590772bccda355255c70
at3158 <tk290@tkfood.com.tw>
2016-08-16 16:43:44 +0800

    add frmACTCompany

---
+++ TKKPI/frmACTCompany.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NPOI;
+using NPOI.HPSF;
+using NPOI.HSSF;
+using NPOI.HSSF.UserModel;
+using NPOI.POIFS;
+using NPOI.Util;
+using NPOI.HSSF.Util;
+using NPOI.HSSF.Extractor;
+using System.IO;
+using System.Data.SqlClient;
+using NPOI.SS.UserModel;
+using System.Configuration;
+using NPOI.XSSF.UserModel;
+
+namespace TKKPI
+{
+    public partial class frmACTCompany : Form
+    {
+        SqlConnection sqlConn = new SqlConnection();
+        SqlCommand sqlComm = new SqlCommand();
+        string connectionString;
+        StringBuilder sbSql = new StringBuilder();
+        StringBuilder sbSqlQuery = new StringBuilder();
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        SqlCommandBuilder sqlCmdBuilder = new SqlCommandBuilder();
+        SqlCommand cmd = new SqlCommand();
+        DataSet ds = new DataSet();
+        DataTable dt = new DataTable();
+        string talbename = null;
+        int rownum = 0;
+
+        public frmACTCompany()
+        {
+            InitializeComponent();
+            DateTime dt = DateTime.Now.AddMonths(-1);
+            dateTimePicker1.Value = dt;
+        }
+
+        #region FUNCTION
+        public void Search()
+        {
+            try
+            {
+                sbSql.Clear();
+                sbSql = SETsbSql();
+
+                if (!string.IsNullOrEmpty(sbSql.ToString()))
+                {
+                    connectionString = ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString;
+                    sqlConn = new SqlConnection(connectionString);
+
+
+
+                    adapter = new SqlDataAdapter(sbSql.ToString(), sqlConn);
+                    sqlCmdBuilder = new SqlCommandBuilder(adapter);
+
+                    sqlConn.Open();
+                    ds.Clear();
+                    adapter.Fill(ds, talbename);
+                    sqlConn.Close();
+
+                    label1.Text = "資料筆數:" + ds.Tables[talbename].Rows.Count.ToString();
+
+                    if (ds.Tables[talbename].Rows.Count == 0)
+                    {
+
+                    }
+                    else
+                    {
+                        dataGridView1.DataSource = ds.Tables[talbename];
+                        dataGridView1.AutoResizeColumns();
+                        rownum = ds.Tables[talbename].Rows.Count - 1;
+                        dataGridView1.CurrentCell = dataGridView1.Rows[rownum].Cells[0];
+
+                        //dataGridView1.CurrentCell = dataGridView1[0, 2];
+
+                    }
+                }
+                else
+                {
+
+                }
+
+
+
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+
+            }
+
+        }
+
+        public StringBuilder SETsbSql()
+        {
+            StringBuilder STR = new StringBuilder();
+            DateTime dt = dateTimePicker1.Value;
+            string ThisYear = null;
+            string ThisMonth = null;
+            string LastMonth = null;
+            string LastYear = null;
+            string LastYearMonth = null;
+
+            ThisYear= dateTimePicker1.Value.ToString("yyyy");
+            ThisMonth = dateTimePicker1.Value.ToString("MM");
+            LastMonth=dt.AddMonths(-1).ToString("MM");
+            LastYear = dt.AddYears(-1).ToString("yyyy");
+            LastYearMonth = dt.AddYears(-1).AddMonths(1).ToString("MM");
+
+            if (comboBox1.Text.ToString().Equals("公司整體指標"))
+            {
+                STR.Append(@"  SELECT 年,月,ROUND(總負債/總資產,2) AS '負債佔資產比率'");
+                STR.Append(@"  ,ROUND(流動資產/流動負債,2) AS '流動比率'");
+                STR.Append(@"  ,ROUND(速動資產/流動負債,2) AS '速動比率'");
+                STR.Append(@"  ,ROUND(本12個月營業收入/((本月期初應收帳款+本月期末應收帳款+本月期初應收票據+本月期末應收票據)/2),2) AS '應收帳款週轉率'");
+                STR.Append(@"  ,CAST (ROUND(365/ROUND(本12個月營業收入/((本月期初應收帳款+本月期末應收帳款+本月期初應收票據+本月期末應收票據)/2),2),2) AS decimal(18,2)) AS '平均收款日數'");
+                STR.Append(@"  ,ROUND(銷貨成本/((期初存貨+期末存貨)/2),2) AS '存貨週轉率'");
+                STR.Append(@"  ,ROUND(銷貨成本/((製成品期初存貨+製成品期末存貨)/2),2) AS '平均製成品週轉率'");
+                STR.Append(@"  ,(CAST (ROUND(365/ROUND(本12個月營業收入/((本月期初應收帳款+本月期末應收帳款+本月期初應收票據+本月期末應收票據)/2),2),2) AS decimal(18,2))+CAST (ROUND(365/ROUND(銷貨成本/((期初存貨+期末存貨)/2),2),2) AS decimal(18,2))) AS '平均銷貨日數'");
+                STR.Append(@"  ,ROUND(銷貨成本/應付帳款餘額,2) AS '應付款項週轉率'");
+                STR.Append(@"  ,CAST (ROUND(365/ROUND(銷貨成本/應付帳款餘額,2),2) AS decimal(18,2)) AS '應付款項週轉天數'");
+                STR.Append(@"  ,CAST (ROUND(365/ROUND(本12個月營業收入/((本月期初應收帳款+本月期末應收帳款+本月期初應收票據+本月期末應收票據)/2),2),2) AS decimal(18,2))-CAST (ROUND(365/ROUND(銷貨成本/應付帳款餘額,2),2) AS decimal(18,2)) AS '應收.付天數'");
+                STR.AppendFormat(@"  ,ROUND((營業收入類/固定資產淨額)/{0}*12,2) AS '固定資產週轉率'",Convert.ToInt16(ThisMonth));
+                STR.AppendFormat(@"  ,ROUND((營業收入類/總資產淨額)/{0}*12,2) AS '總資產週轉率'", Convert.ToInt16(ThisMonth));
+                STR.Append(@"  ,ROUND((當年度本期損利+年利息費用)/總資產淨額,2) AS '資產報酬率'");
+                STR.Append(@"  ,ROUND(當年度本期損利/股東權益,2) AS '權益報酬率(稅後)'");
+                STR.Append(@"  ,ROUND(損益表本期淨利/(股本/10),2) AS '每股盈餘(稅後)'");
+                STR.Append(@"  ,ROUND(當月營業收入類/140,2) AS '每人營業淨額'");
+                STR.Append(@"  ,ROUND((當月損益表本期淨利4+當月損益表本期淨利5+當月損益表本期淨利6)/140,2) AS '每人營業利益'");
+                STR.Append(@"  ,ROUND(總負債/股東權益,2) AS '負債/淨值'");
+                STR.Append(@"  ,ROUND((當月損益表本期淨利4+當月損益表本期淨利5+當月損益表本期淨利6+當月損益表本期淨利7+當期所得稅費用)/利息費用,2) AS '利息保障倍數(稅前)'");
+                STR.Append(@"  FROM (");
+                STR.AppendFormat(@"  SELECT '{0}' AS '年','{1}' AS '月'",ThisYear,ThisMonth);
+                STR.AppendFormat(@"  ,(Select SUM(MB004)-SUM(MB005) From [TK].dbo.ACTMB Where MB001 LIKE '1%' and MB002='{0}' and MB003<='{1}'  AND MB001 IN (SELECT MA001 FROM [TK].dbo.ACTMA WHERE (MA008='2' OR MA008='3'))) AS '總資產'", ThisYear, ThisMonth);
+                STR.AppendFormat(@"  ,(Select SUM(MB005)-SUM(MB004) From [TK].dbo.ACTMB Where MB001 LIKE '2%' and MB002='{0}' and MB003<='{1}'  AND MB001 IN (SELECT MA001 FROM [TK].dbo.ACTMA WHERE (MA008='2' OR MA008='3'))) AS '總負債' ", ThisYear, ThisMonth);
+                STR.AppendFormat(@"  ,(Select SUM(MB004)-SUM(MB005) From [TK].dbo.ACTMB Where MB001 LIKE '1%' and MB002='{0}' and MB003<='{1}'  AND MB001 IN (SELECT MA001 FROM [TK].dbo.ACTMA WHERE (MA008='2' OR MA008='3')) AND MB001 IN (SELECT ID FROM [TKKPI].dbo.ACTCurrentRatio WHERE ID LIKE '1%')) AS '流動資產' ", ThisYear, ThisMonth);
+                STR.AppendFormat(@"  ,(Select SUM(MB005)-SUM(MB004) From [TK].dbo.ACTMB Where MB001 LIKE '2%' and MB002='{0}' and MB003<='{1}'  AND MB001 IN (SELECT MA001 FROM [TK].dbo.ACTMA WHERE (MA008='2' OR MA008='3')) AND MB001 IN (SELECT ID FROM [TKKPI].dbo.ACTCurrentRatio WHERE ID LIKE '2%')) AS '流動負債'", ThisYear, ThisMonth);
+                STR.AppendFormat(@"  ,(Select SUM(MB004)-SUM(MB005) From [TK].dbo.ACTMB Where MB001 LIKE '1%' and MB002='{0}' and MB003<='{1}'  AND MB001 IN (SELECT MA001 FROM [TK].dbo.ACTMA WHERE (MA008='2' OR MA008='3')) AND MB001 IN (SELECT ID FROM [TKKPI].dbo.ACTQuickRatio WHERE ID LIKE '1%')) AS '速動資產'", ThisYear, ThisMonth);
+                STR.AppendFormat(@"  ,(Select SUM(MB005)-SUM(MB004) FROM [TK].dbo.ACTMB WITH (NOLOCK) Where MB001 like'4%' AND MB001 IN (SELECT MA001 FROM [TK].dbo.ACTMA WHERE (MA008='2' OR MA008='3')) and MB002+MB003>='{0}'and MB002+MB003<='{1}') AS '本12個月營業收入'", LastYear+ LastYearMonth, ThisYear+ThisMonth);
+                STR.AppendFormat(@"  ,(Select SUM(MB004)-SUM(MB005) FROM [TK].dbo.ACTMB WITH (NOLOCK) Where MB001 like'115%' AND MB001 IN (SELECT MA001 FROM [TK].dbo.ACTMA WHERE (MA008='2' OR MA008='3')) and MB002='{0}' AND MB003<='{1}') AS '本月期初應收帳款'",ThisYear,LastMonth);
+                STR.AppendFormat(@"  ,(Select SUM(MB004)-SUM(MB005) FROM [TK].dbo.ACTMB WITH (NOLOCK) Where MB001 like'115%' AND MB001 IN (SELECT MA001 FROM [TK].dbo.ACTMA WHERE (MA008='2' OR MA008='3')) and MB002='{0}' AND MB003<='{1}') AS '本月期末應收帳款'",ThisYear,ThisMonth);
+                STR.AppendFormat(@"  ,(Select SUM(MB004)-SUM(MB005) FROM [TK].dbo.ACTMB WITH (NOLOCK) Where MB001 like'117%' AND MB001 IN (SELECT MA001 FROM [TK].dbo.ACTMA WHERE (MA008='2' OR MA008='3')) and MB002='{0}' AND MB003<='{1}') AS '本月期初應收票據'", ThisYear, LastMonth);
+                STR.AppendFormat(@"  ,(Select SUM(MB004)-SUM(MB005) FROM [TK].dbo.ACTMB WITH (NOLOCK) Where MB001 like'117%' AND MB001 IN (SELECT MA001 FROM [TK].dbo.ACTMA WHERE (MA008='2' OR MA008='3')) and MB002='{0}' AND MB003<='{1}') AS '本月期末應收票據'", ThisYear, ThisMonth); 
+                STR.AppendFormat(@"  ,(SELECT SUM(MB004)-SUM(MB005) FROM [TK].dbo.ACTMB WITH (NOLOCK) WHERE  MB001='51111' AND  SUBSTRING(LTRIM(RTRIM(MB002))+LTRIM(RTRIM(MB003)),1,6)>='{0}' AND  SUBSTRING(LTRIM(RTRIM(MB002))+LTRIM(RTRIM(MB003)),1,6)<='{1}') AS '銷貨成本'", LastYear + LastYearMonth, ThisYear + ThisMonth);
+                STR.AppendFormat(@"  ,(Select SUM(MB004)-SUM(MB005) FROM [TK].dbo.ACTMB WITH (NOLOCK) Where MB001 like'13%' and MB002='{0}'and MB003<='{1}') AS '期初存貨'",LastYear,LastYearMonth);
+                STR.AppendFormat(@"  ,(Select SUM(MB004)-SUM(MB005) FROM [TK].dbo.ACTMB WITH (NOLOCK) Where MB001 like'13%' and MB002='{0}'and MB003<='{1}') AS '期末存貨'",ThisYear,ThisMonth);
+                STR.AppendFormat(@"  ,(Select SUM(MB004)-SUM(MB005) FROM [TK].dbo.ACTMB WITH (NOLOCK) Where MB001 like'1311%' and MB002='{0}'and MB003<='{1}') AS '製成品期初存貨'", LastYear, LastYearMonth); ;
+                STR.AppendFormat(@"  ,(Select SUM(MB004)-SUM(MB005) FROM [TK].dbo.ACTMB WITH (NOLOCK) Where MB001 like'1311%' and MB002='{0}'and MB003<='{1}') AS '製成品期末存貨'", ThisYear, ThisMonth);
+                STR.AppendFormat(@"  ,(Select SUM(MB005)-SUM(MB004) FROM [TK].dbo.ACTMB WITH (NOLOCK) Where MB001 like'2171%' and MB002='{0}' and MB003<='{1}') AS '應付帳款餘額'", ThisYear, ThisMonth);
+                STR.AppendFormat(@"  ,(Select SUM(MB005)-SUM(MB004) FROM [TK].dbo.ACTMB WITH (NOLOCK) Where MB001 like'4%' AND MB001 IN (SELECT MA001 FROM [TK].dbo.ACTMA WHERE (MA008='2' OR MA008='3')) and MB002='{0}' and MB003<='{1}') AS '營業收入類'", ThisYear, ThisMonth);
+                STR.AppendFormat(@"  ,(Select SUM(MB004)-SUM(MB005) FROM [TK].dbo.ACTMB WITH (NOLOCK) Where( MB001 like'16%' OR MB001 like'17%' OR MB001 like'191%')AND MB001 IN (SELECT MA001 FROM [TK].dbo.ACTMA WHERE (MA008='2' OR MA008='3')) and MB002='{0}'and MB003<='{1}') AS '固定資產淨額' ", ThisYear, ThisMonth);
+                STR.AppendFormat(@"  ,(Select SUM(MB004)-SUM(MB005) FROM [TK].dbo.ACTMB WITH (NOLOCK) Where MB001 like'1%' AND MB001 IN (SELECT MA001 FROM [TK].dbo.ACTMA WHERE (MA008='2' OR MA008='3')) and MB002='{0}' and MB003<='{1}') AS '總資產淨額' ", ThisYear, ThisMonth);
+                STR.AppendFormat(@"  ,(Select SUM(MB005)-SUM(MB004) FROM [TK].dbo.ACTMB WITH (NOLOCK) Where MB001 like'3353%' AND MB001 IN (SELECT MA001 FROM [TK].dbo.ACTMA WHERE (MA008='2' OR MA008='3')) and MB002='{0}' and MB003<='{1}') AS '當年度本期損利'", ThisYear, ThisMonth);
+                STR.AppendFormat(@"  ,(Select SUM(MB004)-SUM(MB005) FROM [TK].dbo.ACTMB WITH (NOLOCK) Where( MB001 like '7511%')AND MB001 IN (SELECT MA001 FROM [TK].dbo.ACTMA WHERE (MA008='2' OR MA008='3')) and MB002='{0}'and MB003<='{1}') AS '年利息費用'", ThisYear, ThisMonth);
+                STR.AppendFormat(@"  ,(Select SUM(MB005)-SUM(MB004) FROM [TK].dbo.ACTMB WITH (NOLOCK) Where MB001 like'3%' AND MB001 IN (SELECT MA001 FROM [TK].dbo.ACTMA WHERE (MA008='2' OR MA008='3')) and MB002='{0}' and MB003<='{1}') AS '股東權益'", ThisYear, ThisMonth);
+                STR.AppendFormat(@"  ,(Select SUM(MB005)-SUM(MB004) FROM [TK].dbo.ACTMB WITH (NOLOCK) Where MB001 like'3353%' AND MB001 IN (SELECT MA001 FROM [TK].dbo.ACTMA WHERE (MA008='2' OR MA008='3')) and MB002='{0}' and MB003='{1}') '損益表本期淨利'", ThisYear, ThisMonth);
+                STR.AppendFormat(@"  ,(Select SUM(MB005)-SUM(MB004) FROM [TK].dbo.ACTMB WITH (NOLOCK) Where MB001 like'31%' AND MB001 IN (SELECT MA001 FROM [TK].dbo.ACTMA WHERE (MA008='2' OR MA008='3')) and MB002='{0}' and MB003<='{1}') AS '股本'", ThisYear, ThisMonth);
+                STR.AppendFormat(@"  ,(Select SUM(MB005)-SUM(MB004) FROM [TK].dbo.ACTMB WITH (NOLOCK) Where MB001 like'4%' AND MB001 IN (SELECT MA001 FROM [TK].dbo.ACTMA WHERE (MA008='2' OR MA008='3')) and MB002='{0}' and MB003='{1}') AS '當月營業收入類'", ThisYear, ThisMonth);
+                STR.AppendFormat(@"  ,(Select SUM(MB005)-SUM(MB004) FROM [TK].dbo.ACTMB WITH (NOLOCK) Where MB001 like'4%' AND MB001 IN (SELECT MA001 FROM [TK].dbo.ACTMA WHERE (MA008='2' OR MA008='3')) and MB002='{0}' and MB003='{1}') AS '當月損益表本期淨利4'", ThisYear, ThisMonth);
+                STR.AppendFormat(@"  ,(Select SUM(MB005)-SUM(MB004) FROM [TK].dbo.ACTMB WITH (NOLOCK) Where MB001 like'5%' AND MB001 IN (SELECT MA001 FROM [TK].dbo.ACTMA WHERE (MA008='2' OR MA008='3')) and MB002='{0}' and MB003='{1}') AS '當月損益表本期淨利5'", ThisYear, ThisMonth);
+                STR.AppendFormat(@"  ,(Select SUM(MB005)-SUM(MB004) FROM [TK].dbo.ACTMB WITH (NOLOCK)  Where MB001 like'6%' AND MB001 IN (SELECT MA001 FROM [TK].dbo.ACTMA WHERE (MA008='2' OR MA008='3')) and MB002='{0}' and MB003='{1}' ) AS '當月損益表本期淨利6'", ThisYear, ThisMonth);
+                STR.AppendFormat(@"  ,(Select SUM(MB005)-SUM(MB004) FROM [TK].dbo.ACTMB WITH (NOLOCK) Where MB001 like'7%'  AND MB001 IN (SELECT MA001 FROM [TK].dbo.ACTMA WHERE (MA008='2' OR MA008='3'))  and MB002='{0}' and MB003='{1}' ) AS '當月損益表本期淨利7'", ThisYear, ThisMonth);
+                STR.AppendFormat(@"  ,(Select SUM(MB004)-SUM(MB005) FROM [TK].dbo.ACTMB WITH (NOLOCK) Where MB001 like'7951%' AND MB001 IN (SELECT MA001 FROM [TK].dbo.ACTMA WHERE (MA008='2' OR MA008='3')) and MB002='{0}' and MB003='{1}') AS '當期所得稅費用'", ThisYear, ThisMonth);
+                STR.AppendFormat(@"  ,(Select SUM(MB004)-SUM(MB005) FROM [TK].dbo.ACTMB WITH (NOLOCK) Where MB001 like'7511%' AND MB001 IN (SELECT MA001 FROM [TK].dbo.ACTMA WHERE (MA008='2' OR MA008='3')) and MB002='{0}' and MB003='{1}') AS '利息費用'", ThisYear, ThisMonth);
+                STR.Append(@"  ) AS TEMP");
+                STR.Append(@"  ");
+
+
+                talbename = "TEMPds1";
+            }
+          
+            
+
+
+            return STR;
+        }
+
+        public void ExcelExport()
+        {
+            Search();
+
+            //建立Excel 2003檔案
+            IWorkbook wb = new XSSFWorkbook();
+            ISheet ws;
+
+
+            dt = ds.Tables[talbename];
+            if (dt.TableName != string.Empty)
+            {
+                ws = wb.CreateSheet(dt.TableName);
+            }
+            else
+            {
+                ws = wb.CreateSheet("Sheet1");
+            }
+
+            ws.CreateRow(0);//第一行為欄位名稱
+            for (int i = 0; i < dt.Columns.Count; i++)
+            {
+                ws.GetRow(0).CreateCell(i).SetCellValue(dt.Columns[i].ColumnName);
+            }
+
+
+            int j = 0;
+            foreach (DataGridViewRow dr in this.dataGridView1.Rows)
+            {
+                ws.CreateRow(j + 1);
+                ws.GetRow(j + 1).CreateCell(0).SetCellValue(((System.Data.DataRowView)(dr.DataBoundItem)).Row.ItemArray[0].ToString());
+                ws.GetRow(j + 1).CreateCell(1).SetCellValue(((System.Data.DataRowView)(dr.DataBoundItem)).Row.ItemArray[1].ToString());
+                ws.GetRow(j + 1).CreateCell(2).SetCellValue(Convert.ToDouble(((System.Data.DataRowView)(dr.DataBoundItem)).Row.ItemArray[2].ToString()));
+                ws.GetRow(j + 1).CreateCell(3).SetCellValue(Convert.ToDouble(((System.Data.DataRowView)(dr.DataBoundItem)).Row.ItemArray[3].ToString()));
+                ws.GetRow(j + 1).CreateCell(4).SetCellValue(Convert.ToDouble(((System.Data.DataRowView)(dr.DataBoundItem)).Row.ItemArray[4].ToString()));
+                ws.GetRow(j + 1).CreateCell(5).SetCellValue(Convert.ToDouble(((System.Data.DataRowView)(dr.DataBoundItem)).Row.ItemArray[5].ToString()));
+                ws.GetRow(j + 1).CreateCell(6).SetCellValue(Convert.ToDouble(((System.Data.DataRowView)(dr.DataBoundItem)).Row.ItemArray[6].ToString()));
+                ws.GetRow(j + 1).CreateCell(7).SetCellValue(Convert.ToDouble(((System.Data.DataRowView)(dr.DataBoundItem)).Row.ItemArray[7].ToString()));
+                ws.GetRow(j + 1).CreateCell(8).SetCellValue(Convert.ToDouble(((System.Data.DataRowView)(dr.DataBoundItem)).Row.ItemArray[8].ToString()));
+                ws.GetRow(j + 1).CreateCell(9).SetCellValue(Convert.ToDouble(((System.Data.DataRowView)(dr.DataBoundItem)).Row.ItemArray[9].ToString()));
+                ws.GetRow(j + 1).CreateCell(10).SetCellValue(Convert.ToDouble(((System.Data.DataRowView)(dr.DataBoundItem)).Row.ItemArray[10].ToString()));
+                ws.GetRow(j + 1).CreateCell(11).SetCellValue(Convert.ToDouble(((System.Data.DataRowView)(dr.DataBoundItem)).Row.ItemArray[11].ToString()));
+                ws.GetRow(j + 1).CreateCell(12).SetCellValue(Convert.ToDouble(((System.Data.DataRowView)(dr.DataBoundItem)).Row.ItemArray[12].ToString()));
+                ws.GetRow(j + 1).CreateCell(13).SetCellValue(Convert.ToDouble(((System.Data.DataRowView)(dr.DataBoundItem)).Row.ItemArray[13].ToString()));
+                ws.GetRow(j + 1).CreateCell(14).SetCellValue(Convert.ToDouble(((System.Data.DataRowView)(dr.DataBoundItem)).Row.ItemArray[14].ToString()));
+                ws.GetRow(j + 1).CreateCell(15).SetCellValue(Convert.ToDouble(((System.Data.DataRowView)(dr.DataBoundItem)).Row.ItemArray[15].ToString()));
+                ws.GetRow(j + 1).CreateCell(16).SetCellValue(Convert.ToDouble(((System.Data.DataRowView)(dr.DataBoundItem)).Row.ItemArray[16].ToString()));
+                ws.GetRow(j + 1).CreateCell(17).SetCellValue(Convert.ToDouble(((System.Data.DataRowView)(dr.DataBoundItem)).Row.ItemArray[17].ToString()));
+                ws.GetRow(j + 1).CreateCell(18).SetCellValue(Convert.ToDouble(((System.Data.DataRowView)(dr.DataBoundItem)).Row.ItemArray[18].ToString()));
+                ws.GetRow(j + 1).CreateCell(19).SetCellValue(Convert.ToDouble(((System.Data.DataRowView)(dr.DataBoundItem)).Row.ItemArray[19].ToString()));
+                ws.GetRow(j + 1).CreateCell(20).SetCellValue(Convert.ToDouble(((System.Data.DataRowView)(dr.DataBoundItem)).Row.ItemArray[20].ToString()));
+                ws.GetRow(j + 1).CreateCell(21).SetCellValue(Convert.ToDouble(((System.Data.DataRowView)(dr.DataBoundItem)).Row.ItemArray[21].ToString()));
+                
+
+
+                j++;
+            }
+
+            if (Directory.Exists(@"c:\temp\"))
+            {
+                //資料夾存在
+            }
+            else
+            {
+                //新增資料夾
+                Directory.CreateDirectory(@"c:\temp\");
+            }
+            StringBuilder filename = new StringBuilder();
+            filename.AppendFormat(@"c:\temp\公司整體指標{0}.xlsx", DateTime.Now.ToString("yyyyMMdd"));
+
+            FileStream file = new FileStream(filename.ToString(), FileMode.Create);//產生檔案
+            wb.Write(file);
+            file.Close();
+
+            MessageBox.Show("匯出完成-EXCEL放在-" + filename.ToString());
+            FileInfo fi = new FileInfo(filename.ToString());
+            if (fi.Exists)
+            {
+                System.Diagnostics.Process.Start(filename.ToString());
+            }
+            else
+            {
+                //file doesn't exist
+            }
+
+
+        }
+
+
+        #endregion
+
+        #region BUTTON
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Search();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ExcelExport();
+        }
+        #endregion
+
+
+    }
+}
