fb9d081690721132b957d71d7db8bf28550e1761
at3158 <tk290@tkfood.com.tw>
2016-08-17 14:48:01 +0800

    ADD frmCOPSell

---
+++ TKKPI/frmCOPSell.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NPOI;
+using NPOI.HPSF;
+using NPOI.HSSF;
+using NPOI.HSSF.UserModel;
+using NPOI.POIFS;
+using NPOI.Util;
+using NPOI.HSSF.Util;
+using NPOI.HSSF.Extractor;
+using System.IO;
+using System.Data.SqlClient;
+using NPOI.SS.UserModel;
+using System.Configuration;
+using NPOI.XSSF.UserModel;
+
+namespace TKKPI
+{
+    public partial class frmCOPSell : Form
+    {
+        SqlConnection sqlConn = new SqlConnection();
+        SqlCommand sqlComm = new SqlCommand();
+        string connectionString;
+        StringBuilder sbSql = new StringBuilder();
+        StringBuilder sbSqlQuery = new StringBuilder();
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        SqlCommandBuilder sqlCmdBuilder = new SqlCommandBuilder();
+        SqlCommand cmd = new SqlCommand();
+        DataSet ds = new DataSet();
+        DataTable dt = new DataTable();
+        string tablename = null;
+        int rownum = 0;
+       
+
+        public frmCOPSell()
+        {
+            InitializeComponent();
+        }
+
+        #region FUNCTION
+       
+
+        public void Search()
+        {
+            try
+            {
+                sbSql.Clear();
+                sbSql = SETsbSql();
+
+                if (!string.IsNullOrEmpty(sbSql.ToString()))
+                {
+                    connectionString = ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString;
+                    sqlConn = new SqlConnection(connectionString);
+
+
+
+                    adapter = new SqlDataAdapter(sbSql.ToString(), sqlConn);
+                    sqlCmdBuilder = new SqlCommandBuilder(adapter);
+
+                    sqlConn.Open();
+                    ds.Clear();
+                    adapter.Fill(ds, tablename);
+                    sqlConn.Close();
+
+                    label1.Text = "資料筆數:" + ds.Tables[tablename].Rows.Count.ToString();
+
+                    if (ds.Tables[tablename].Rows.Count == 0)
+                    {
+
+                    }
+                    else
+                    {
+                        dataGridView1.DataSource = ds.Tables[tablename];
+                        dataGridView1.AutoResizeColumns();
+                        //rownum = ds.Tables[tablename].Rows.Count - 1;
+                        dataGridView1.CurrentCell = dataGridView1.Rows[rownum].Cells[0];
+
+                        //dataGridView1.CurrentCell = dataGridView1[0, 2];
+
+                    }
+                }
+                else
+                {
+
+                }
+
+
+
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+
+            }
+
+        }
+
+        public StringBuilder SETsbSql()
+        {
+            StringBuilder STR = new StringBuilder();
+            DateTime dt = dateTimePicker1.Value;
+            string ThisYear = null;
+            string ThisMonth = null;
+            string LastMonth = null;
+            string LastYear = null;
+            string LastYearMonth = null;
+
+            ThisYear = dateTimePicker1.Value.ToString("yyyy");
+            ThisMonth = dateTimePicker1.Value.ToString("MM");
+            LastMonth = dt.AddMonths(-1).ToString("MM");
+            LastYear = dt.AddYears(-1).ToString("yyyy");
+            LastYearMonth = dt.AddYears(-1).AddMonths(1).ToString("MM");
+
+            if (comboBox1.Text.ToString().Equals("業務業績表"))
+            {
+                STR.Append(@"  SELECT 年月,業務,銷貨數量,銷退數量,銷貨金額,銷退金額");
+                STR.Append(@"  FROM (");
+                STR.Append(@"  SELECT ID AS '月份'");
+                STR.AppendFormat(@"  ,'{0}' +ID  AS  '年月'",dateTimePicker1.Value.ToString("yyyy"));
+                STR.Append(@"  ,'業務部' AS '業務'");
+                STR.AppendFormat(@"  ,CAST(ISNULL((SELECT SUM(LA011) FROM [TK].dbo.COPTG,[TK].dbo.COPTH,[TK].dbo.INVLA WHERE TG001=TH001 AND TG002=TH002 AND LA006=TH001 AND LA007=TH002 AND LA008=TH003 AND SUBSTRING(TH002,1,6)='{0}'+ID AND TG006 IN ('070005','090002','140020','140049','140078')),0) AS INT) AS '銷貨數量' ", dateTimePicker1.Value.ToString("yyyy"));
+                STR.AppendFormat(@"  ,CAST(ISNULL((SELECT SUM(TH013) FROM [TK].dbo.COPTG,[TK].dbo.COPTH WHERE TG001=TH001 AND TG002=TH002  AND SUBSTRING(TH002,1,6)='{0}'+ID AND TG006 IN ('070005','090002','140020','140049','140078')),0) AS INT) AS '銷貨金額' ", dateTimePicker1.Value.ToString("yyyy"));
+                STR.AppendFormat(@"  ,CAST(ISNULL((SELECT SUM(LA011) FROM [TK].dbo.COPTI,[TK].dbo.COPTJ,[TK].dbo.INVLA WHERE TI001=TJ001 AND TI002=TJ002 AND LA006=TJ001 AND LA007=TJ002 AND LA008=TJ003 AND SUBSTRING(TJ002,1,6)='{0}'+ID AND TI006           IN ('070005','090002','140020','140049','140078')),0) AS INT) AS '銷退數量' ", dateTimePicker1.Value.ToString("yyyy"));
+                STR.AppendFormat(@"  ,CAST(ISNULL((SELECT SUM(TJ012) FROM [TK].dbo.COPTI,[TK].dbo.COPTJ WHERE TI001=TJ001 AND TI002=TJ002  AND SUBSTRING(TJ002,1,6)='{0}'+ID AND TI006           IN ('070005','090002','140020','140049','140078')),0) AS INT) AS '銷退金額' ", dateTimePicker1.Value.ToString("yyyy"));
+                STR.Append(@"  FROM[TKECOMMERCE].dbo.BASEMONTH");
+                STR.Append(@"  UNION ALL ");
+                STR.Append(@"  SELECT ID AS '月份'");
+                STR.AppendFormat(@"  ,'{0}'+ID  AS  '年月'", dateTimePicker1.Value.ToString("yyyy"));
+                STR.Append(@"  ,'劉莉琴' AS '業務'");
+                STR.AppendFormat(@"  ,CAST(ISNULL((SELECT SUM(LA011) FROM [TK].dbo.COPTG,[TK].dbo.COPTH,[TK].dbo.INVLA WHERE TG001=TH001 AND TG002=TH002 AND LA006=TH001 AND LA007=TH002 AND LA008=TH003 AND SUBSTRING(TH002,1,6)='{0}'+ID AND TG006 IN ('140049')),0) AS INT) AS '銷貨數量' ", dateTimePicker1.Value.ToString("yyyy"));
+                STR.AppendFormat(@"  ,CAST(ISNULL((SELECT SUM(TH013) FROM [TK].dbo.COPTG,[TK].dbo.COPTH WHERE TG001=TH001 AND TG002=TH002  AND SUBSTRING(TH002,1,6)='{0}'+ID AND TG006 IN ('140049')),0) AS INT) AS '銷貨金額' ", dateTimePicker1.Value.ToString("yyyy"));
+                STR.AppendFormat(@"  ,CAST(ISNULL((SELECT SUM(LA011) FROM [TK].dbo.COPTI,[TK].dbo.COPTJ,[TK].dbo.INVLA WHERE TI001=TJ001 AND TI002=TJ002 AND LA006=TJ001 AND LA007=TJ002 AND LA008=TJ003 AND SUBSTRING(TJ002,1,6)='{0}'+ID AND TI006           IN ('140049')),0) AS INT) AS '銷退數量' ", dateTimePicker1.Value.ToString("yyyy"));
+                STR.AppendFormat(@"  ,CAST(ISNULL((SELECT SUM(TJ012) FROM [TK].dbo.COPTI,[TK].dbo.COPTJ WHERE TI001=TJ001 AND TI002=TJ002  AND SUBSTRING(TJ002,1,6)='{0}'+ID AND TI006  IN ('140049')),0) AS INT) AS '銷退金額' ", dateTimePicker1.Value.ToString("yyyy"));
+                STR.Append(@"  FROM [TKECOMMERCE].dbo.BASEMONTH");
+                STR.Append(@"  UNION ALL");
+                STR.Append(@"  SELECT ID AS '月份'");
+                STR.AppendFormat(@"  ,'{0}'+ID  AS  '年月'", dateTimePicker1.Value.ToString("yyyy"));
+                STR.Append(@"  ,'林智祥' AS '業務' ");
+                STR.AppendFormat(@"  ,CAST(ISNULL((SELECT SUM(LA011) FROM [TK].dbo.COPTG,[TK].dbo.COPTH,[TK].dbo.INVLA WHERE TG001=TH001 AND TG002=TH002 AND LA006=TH001 AND LA007=TH002 AND LA008=TH003 AND SUBSTRING(TH002,1,6)='{0}'+ID AND TG006 IN ('090002')),0) AS INT) AS '銷貨數量' ", dateTimePicker1.Value.ToString("yyyy"));
+                STR.AppendFormat(@"  ,CAST(ISNULL((SELECT SUM(TH013) FROM [TK].dbo.COPTG,[TK].dbo.COPTH WHERE TG001=TH001 AND TG002=TH002  AND SUBSTRING(TH002,1,6)='{0}'+ID AND TG006 IN ('090002')),0) AS INT) AS '銷貨金額' ", dateTimePicker1.Value.ToString("yyyy")); ;
+                STR.AppendFormat(@"  ,CAST(ISNULL((SELECT SUM(LA011) FROM [TK].dbo.COPTI,[TK].dbo.COPTJ,[TK].dbo.INVLA WHERE TI001=TJ001 AND TI002=TJ002 AND LA006=TJ001 AND LA007=TJ002 AND LA008=TJ003 AND SUBSTRING(TJ002,1,6)='{0}'+ID AND TI006           IN ('090002')),0) AS INT) AS '銷退數量' ", dateTimePicker1.Value.ToString("yyyy"));
+                STR.AppendFormat(@"  ,CAST(ISNULL((SELECT SUM(TJ012) FROM [TK].dbo.COPTI,[TK].dbo.COPTJ WHERE TI001=TJ001 AND TI002=TJ002  AND SUBSTRING(TJ002,1,6)='{0}'+ID AND TI006  IN ('090002')),0) AS INT) AS '銷退金額' ", dateTimePicker1.Value.ToString("yyyy"));
+                STR.Append(@"  FROM [TKECOMMERCE].dbo.BASEMONTH");
+                STR.Append(@"  UNION ALL ");
+                STR.Append(@"  SELECT ID AS '月份' ");
+                STR.AppendFormat(@"  ,'{0}'+ID  AS  '年月'", dateTimePicker1.Value.ToString("yyyy"));
+                STR.Append(@"  ,'陳玟文' AS '業務'");
+                STR.AppendFormat(@"  ,CAST(ISNULL((SELECT SUM(LA011) FROM [TK].dbo.COPTG,[TK].dbo.COPTH,[TK].dbo.INVLA WHERE TG001=TH001 AND TG002=TH002 AND LA006=TH001 AND LA007=TH002 AND LA008=TH003 AND SUBSTRING(TH002,1,6)='{0}'+ID AND TG006 IN ('140020')),0) AS INT) AS '銷貨數量' ", dateTimePicker1.Value.ToString("yyyy")); ;
+                STR.AppendFormat(@"  ,CAST(ISNULL((SELECT SUM(TH013) FROM [TK].dbo.COPTG,[TK].dbo.COPTH WHERE TG001=TH001 AND TG002=TH002  AND SUBSTRING(TH002,1,6)='{0}'+ID AND TG006 IN ('140020')),0) AS INT) AS '銷貨金額' ", dateTimePicker1.Value.ToString("yyyy"));
+                STR.AppendFormat(@"  ,CAST(ISNULL((SELECT SUM(LA011) FROM [TK].dbo.COPTI,[TK].dbo.COPTJ,[TK].dbo.INVLA WHERE TI001=TJ001 AND TI002=TJ002 AND LA006=TJ001 AND LA007=TJ002 AND LA008=TJ003 AND SUBSTRING(TJ002,1,6)='{0}'+ID AND TI006           IN ('140020')),0) AS INT) AS '銷退數量' ", dateTimePicker1.Value.ToString("yyyy"));
+                STR.AppendFormat(@"  ,CAST(ISNULL((SELECT SUM(TJ012) FROM [TK].dbo.COPTI,[TK].dbo.COPTJ WHERE TI001=TJ001 AND TI002=TJ002  AND SUBSTRING(TJ002,1,6)='{0}'+ID AND TI006  IN ('140020')),0) AS INT) AS '銷退金額' ", dateTimePicker1.Value.ToString("yyyy"));
+                STR.Append(@"  FROM [TKECOMMERCE].dbo.BASEMONTH");
+                STR.Append(@"  UNION ALL ");
+                STR.Append(@"  SELECT ID AS '月份'");
+                STR.AppendFormat(@"  ,'{0}'+ID  AS  '年月'", dateTimePicker1.Value.ToString("yyyy"));
+                STR.Append(@"  ,'蔡顏鴻' AS '業務'");
+                STR.AppendFormat(@"  ,CAST(ISNULL((SELECT SUM(LA011) FROM [TK].dbo.COPTG,[TK].dbo.COPTH,[TK].dbo.INVLA WHERE TG001=TH001 AND TG002=TH002 AND LA006=TH001 AND LA007=TH002 AND LA008=TH003 AND SUBSTRING(TH002,1,6)='{0}'+ID AND TG006 IN ('140078')),0) AS INT) AS '銷貨數量' ", dateTimePicker1.Value.ToString("yyyy")); ;
+                STR.AppendFormat(@"  ,CAST(ISNULL((SELECT SUM(TH013) FROM [TK].dbo.COPTG,[TK].dbo.COPTH WHERE TG001=TH001 AND TG002=TH002  AND SUBSTRING(TH002,1,6)='{0}'+ID AND TG006 IN ('140078')),0) AS INT) AS '銷貨金額' ", dateTimePicker1.Value.ToString("yyyy"));
+                STR.AppendFormat(@"  ,CAST(ISNULL((SELECT SUM(LA011) FROM [TK].dbo.COPTI,[TK].dbo.COPTJ,[TK].dbo.INVLA WHERE TI001=TJ001 AND TI002=TJ002 AND LA006=TJ001 AND LA007=TJ002 AND LA008=TJ003 AND SUBSTRING(TJ002,1,6)='{0}'+ID AND TI006 IN ('140078')),0) AS INT) AS '銷退數量' ", dateTimePicker1.Value.ToString("yyyy"));
+                STR.AppendFormat(@"  ,CAST(ISNULL((SELECT SUM(TJ012) FROM [TK].dbo.COPTI,[TK].dbo.COPTJ WHERE TI001=TJ001 AND TI002=TJ002  AND SUBSTRING(TJ002,1,6)='{0}'+ID AND TI006  IN ('140078')),0) AS INT) AS '銷退金額' ", dateTimePicker1.Value.ToString("yyyy"));
+                STR.Append(@"  FROM [TKECOMMERCE].dbo.BASEMONTH");
+                STR.Append(@"  UNION ALL ");
+                STR.Append(@"  SELECT ID AS '月份'");
+                STR.AppendFormat(@"  ,'{0}'+ID  AS  '年月'", dateTimePicker1.Value.ToString("yyyy")); ;
+                STR.Append(@"  ,'羅家鳳' AS '業務'");
+                STR.AppendFormat(@"  ,CAST(ISNULL((SELECT SUM(LA011) FROM [TK].dbo.COPTG,[TK].dbo.COPTH,[TK].dbo.INVLA WHERE TG001=TH001 AND TG002=TH002 AND LA006=TH001 AND LA007=TH002 AND LA008=TH003 AND SUBSTRING(TH002,1,6)='{0}'+ID AND TG006 IN ('070005')),0) AS INT) AS '銷貨數量' ", dateTimePicker1.Value.ToString("yyyy")); ;
+                STR.AppendFormat(@"  ,CAST(ISNULL((SELECT SUM(TH013) FROM [TK].dbo.COPTG,[TK].dbo.COPTH WHERE TG001=TH001 AND TG002=TH002  AND SUBSTRING(TH002,1,6)='{0}'+ID AND TG006 IN ('070005')),0) AS INT) AS '銷貨金額' ", dateTimePicker1.Value.ToString("yyyy"));
+                STR.AppendFormat(@"  ,CAST(ISNULL((SELECT SUM(LA011) FROM [TK].dbo.COPTI,[TK].dbo.COPTJ,[TK].dbo.INVLA WHERE TI001=TJ001 AND TI002=TJ002 AND LA006=TJ001 AND LA007=TJ002 AND LA008=TJ003 AND SUBSTRING(TJ002,1,6)='{0}'+ID AND TI006           IN ('070005')),0) AS INT) AS '銷退數量' ", dateTimePicker1.Value.ToString("yyyy"));
+                STR.AppendFormat(@"  ,CAST(ISNULL((SELECT SUM(TJ012) FROM [TK].dbo.COPTI,[TK].dbo.COPTJ WHERE TI001=TJ001 AND TI002=TJ002  AND SUBSTRING(TJ002,1,6)='{0}'+ID AND TI006  IN ('070005')),0) AS INT) AS '銷退金額' ", dateTimePicker1.Value.ToString("yyyy"));
+                STR.Append(@"  FROM [TKECOMMERCE].dbo.BASEMONTH");
+                STR.Append(@"  ) AS TEMP ");
+                STR.Append(@"  ");
+
+                tablename = "TEMPds1";
+            }
+           
+
+            return STR;
+        }
+
+        public void ExcelExport()
+        {
+            Search();
+
+            //建立Excel 2003檔案
+            IWorkbook wb = new XSSFWorkbook();
+            ISheet ws;
+
+
+            dt = ds.Tables[tablename];
+            if (dt.TableName != string.Empty)
+            {
+                ws = wb.CreateSheet(dt.TableName);
+            }
+            else
+            {
+                ws = wb.CreateSheet("Sheet1");
+            }
+
+            ws.CreateRow(0);//第一行為欄位名稱
+            for (int i = 0; i < dt.Columns.Count; i++)
+            {
+                ws.GetRow(0).CreateCell(i).SetCellValue(dt.Columns[i].ColumnName);
+            }
+
+
+            int j = 0;
+            if (tablename.Equals("TEMPds1"))
+            {
+                foreach (DataGridViewRow dr in this.dataGridView1.Rows)
+                {
+                    ws.CreateRow(j + 1);
+                    ws.GetRow(j + 1).CreateCell(0).SetCellValue(((System.Data.DataRowView)(dr.DataBoundItem)).Row.ItemArray[0].ToString());
+                    ws.GetRow(j + 1).CreateCell(1).SetCellValue(((System.Data.DataRowView)(dr.DataBoundItem)).Row.ItemArray[1].ToString());
+                    ws.GetRow(j + 1).CreateCell(2).SetCellValue(Convert.ToDouble(((System.Data.DataRowView)(dr.DataBoundItem)).Row.ItemArray[2].ToString()));
+                    ws.GetRow(j + 1).CreateCell(3).SetCellValue(Convert.ToDouble(((System.Data.DataRowView)(dr.DataBoundItem)).Row.ItemArray[3].ToString()));
+                    ws.GetRow(j + 1).CreateCell(4).SetCellValue(Convert.ToDouble(((System.Data.DataRowView)(dr.DataBoundItem)).Row.ItemArray[4].ToString()));
+                    ws.GetRow(j + 1).CreateCell(5).SetCellValue(Convert.ToDouble(((System.Data.DataRowView)(dr.DataBoundItem)).Row.ItemArray[5].ToString()));
+
+                    j++;
+                }
+
+            }
+            
+
+            if (Directory.Exists(@"c:\temp\"))
+            {
+                //資料夾存在
+            }
+            else
+            {
+                //新增資料夾
+                Directory.CreateDirectory(@"c:\temp\");
+            }
+            StringBuilder filename = new StringBuilder();
+            filename.AppendFormat(@"c:\temp\業務指標'{0}'.xlsx", DateTime.Now.ToString("yyyyMMdd"));
+
+            FileStream file = new FileStream(filename.ToString(), FileMode.Create);//產生檔案
+            wb.Write(file);
+            file.Close();
+
+            MessageBox.Show("匯出完成-EXCEL放在-" + filename.ToString());
+            FileInfo fi = new FileInfo(filename.ToString());
+            if (fi.Exists)
+            {
+                System.Diagnostics.Process.Start(filename.ToString());
+            }
+            else
+            {
+                //file doesn't exist
+            }
+
+
+        }
+
+
+        #endregion
+
+        #region BUTTON
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Search();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ExcelExport();
+        }
+        #endregion
+    }
+}
