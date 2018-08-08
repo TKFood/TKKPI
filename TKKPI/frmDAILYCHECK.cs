a04ec4b501c424f6454d4678ec7e0300e0016717
at3158 <tk290@tkfood.com.tw>
2018-08-08 10:55:38 +0800

    Search

---
+++ TKKPI/frmDAILYCHECK.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NPOI;
+using NPOI.HPSF;
+using NPOI.HSSF;
+using NPOI.HSSF.UserModel;
+using NPOI.POIFS;
+using NPOI.Util;
+using NPOI.HSSF.Util;
+using NPOI.HSSF.Extractor;
+using System.IO;
+using System.Data.SqlClient;
+using NPOI.SS.UserModel;
+using System.Configuration;
+using NPOI.XSSF.UserModel;
+
+namespace TKKPI
+{
+    public partial class frmDAILYCHECK : Form
+    {
+        SqlConnection sqlConn = new SqlConnection();
+        SqlCommand sqlComm = new SqlCommand();
+        string connectionString;
+
+        StringBuilder sbSql = new StringBuilder();
+        StringBuilder sbSqlQuery = new StringBuilder();
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        SqlCommandBuilder sqlCmdBuilder = new SqlCommandBuilder();
+        SqlCommand cmd = new SqlCommand();
+        DataSet ds = new DataSet();
+        DataTable dt = new DataTable();
+
+        string talbename = null;
+        int rownum = 0;
+
+        public frmDAILYCHECK()
+        {
+            InitializeComponent();
+        }
+
+        #region FUNCTION
+        public void Search()
+        {
+            try
+            {
+                talbename = "TEMPds1";
+                sbSql.Clear();
+
+                sbSql.AppendFormat(@"  SELECT TD011,ISNULL(TB009,0) TB009,TC053,TD001,TD002,TD003,TD004,TD005,TD005,TD008,TD009,TD010,TD011,TD012,TD013,TD017,TD018,TD019,'',TB004,TB005,TB006,TB007,TB008,TB009,TB010");
+                sbSql.AppendFormat(@"  FROM [TK].dbo.COPTC,[TK].dbo.COPTD");
+                sbSql.AppendFormat(@"  LEFT JOIN [TK].dbo.COPTB ON TB001=TD017 AND TB002=TD018 AND TB003=TD019");
+                sbSql.AppendFormat(@"  WHERE TC001=TD001 AND TC002=TD002");
+                sbSql.AppendFormat(@"  AND COPTD.MODIFIER='160115'");
+                sbSql.AppendFormat(@"  AND TD002 LIKE '{0}%'",dateTimePicker1.Value.ToString("yyyyMMdd"));
+                sbSql.AppendFormat(@"  AND TD011<>ISNULL(TB009,0) ");
+                sbSql.AppendFormat(@"  ");
+
+                if (!string.IsNullOrEmpty(sbSql.ToString()))
+                {
+                    connectionString = ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString;
+                    sqlConn = new SqlConnection(connectionString);
+
+
+
+                    adapter = new SqlDataAdapter(sbSql.ToString(), sqlConn);
+                    sqlCmdBuilder = new SqlCommandBuilder(adapter);
+
+                    sqlConn.Open();
+                    ds.Clear();
+                    adapter.Fill(ds, talbename);
+                    sqlConn.Close();
+
+                   
+                    if (ds.Tables[talbename].Rows.Count == 0)
+                    {
+
+                    }
+                    else
+                    {
+                        dataGridView1.DataSource = ds.Tables[talbename];
+                        dataGridView1.AutoResizeColumns();
+                        //rownum = ds.Tables[talbename].Rows.Count - 1;
+                        dataGridView1.CurrentCell = dataGridView1.Rows[rownum].Cells[0];
+
+                        //dataGridView1.CurrentCell = dataGridView1[0, 2];
+
+                    }
+                }
+                else
+                {
+
+                }
+
+
+
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+
+            }
+
+        }
+
+        #endregion
+
+        #region BUTTON
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Search();
+        }
+
+        #endregion
+    }
+}
