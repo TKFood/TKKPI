6177b24a7da81278050ebfbce0411c7b120b41c0
at3158 <tk290@tkfood.com.tw>
2021-09-17 15:37:11 +0800

    frmPOSSell

--- TKKPI/frmPOSSell.cs
+++ TKKPI/frmPOSSell.cs
@@ -21,6 +21,7 @@ using System.Data.SqlClient;
 using NPOI.SS.UserModel;
 using System.Configuration;
 using NPOI.XSSF.UserModel;
+using TKITDLL;
 
 namespace TKKPI
 {
@@ -54,8 +55,17 @@ namespace TKKPI
 
                 if (!string.IsNullOrEmpty(sbSql.ToString()))
                 {
-                    connectionString = ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString;
-                    sqlConn = new SqlConnection(connectionString);
+                    //20210902密
+                    Class1 TKID = new Class1();//用new 建立類別實體
+                    SqlConnectionStringBuilder sqlsb = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString);
+
+                    //資料庫使用者密碼解密
+                    sqlsb.Password = TKID.Decryption(sqlsb.Password);
+                    sqlsb.UserID = TKID.Decryption(sqlsb.UserID);
+
+                    String connectionString;
+                    sqlConn = new SqlConnection(sqlsb.ConnectionString);
+
 
 
 
