9747e343bf2eed32c4a57ca7473049c7ed1a9286
at3158 <tk290@tkfood.com.tw>
2022-02-15 17:22:17 +0800

    ADDTBINVLACHECK

--- TKKPI/frmREPORTINVLACHECK.cs
+++ TKKPI/frmREPORTINVLACHECK.cs
@@ -42,7 +42,9 @@ namespace TKKPI
         DataTable dt = new DataTable();
         string tablename = null;
         int rownum = 0;
-
+        SqlTransaction tran;
+        DataSet ds1 = new DataSet();
+        int result;
 
         public frmREPORTINVLACHECK()
         {
@@ -51,12 +53,90 @@ namespace TKKPI
 
         #region FUNCTION
 
+        public void ADDTBINVLACHECK()
+        {
+            int days = new TimeSpan(dateTimePicker2.Value.Ticks - dateTimePicker1.Value.Ticks).Days;
+            days = days + 1;
+            DateTime dt = new DateTime();
+            dt = dateTimePicker1.Value;
+
+            try
+            {
+                //20210902密
+                Class1 TKID = new Class1();//用new 建立類別實體
+                SqlConnectionStringBuilder sqlsb = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString);
+
+                //資料庫使用者密碼解密
+                sqlsb.Password = TKID.Decryption(sqlsb.Password);
+                sqlsb.UserID = TKID.Decryption(sqlsb.UserID);
+
+                String connectionString;
+                sqlConn = new SqlConnection(sqlsb.ConnectionString);
+
+
+
+                sqlConn.Close();
+                sqlConn.Open();
+                tran = sqlConn.BeginTransaction();
+
+                sbSql.Clear();
+
+                for(int i=0;i<=days ;i++)
+                {
+
+                    sbSql.AppendFormat(@"  
+                                    INSERT INTO [TKKPI].[dbo].[TBINVLACHECK]
+                                    ([SDATE],[LA009],[LA001],[MB002],[NUMS])
+                                    (SELECT '{0}',LA009,LA001,MB002,ISNULL(SUM(LA005*LA011),0) AS 'NUMS'
+                                    FROM [TK].dbo.INVLA WITH (NOLOCK) ,[TK].dbo.INVMB WITH (NOLOCK) 
+                                    WHERE LA009='20001'
+                                    AND LA001=MB001
+                                    AND LA001='40100210810016'
+                                    AND LA004<='{0}'
+                                    GROUP BY LA009,LA001,MB002)
+                                    ", dt.ToString("yyyyMMdd"));
+
+                    dt = dt.AddDays(1);
+                }
+                
+
+                cmd.Connection = sqlConn;
+                cmd.CommandTimeout = 60;
+                cmd.CommandText = sbSql.ToString();
+                cmd.Transaction = tran;
+                result = cmd.ExecuteNonQuery();
+
+                if (result == 0)
+                {
+                    tran.Rollback();    //交易取消
+
+                    MessageBox.Show("失敗");
+                }
+                else
+                {
+                    tran.Commit();      //執行交易  
+
+                    MessageBox.Show("成功");
+                }
+
+            }
+            catch
+            {
+
+            }
+
+            finally
+            {
+                sqlConn.Close();
+            }
+        }
+
         #endregion
 
         #region BUTTON
         private void button4_Click(object sender, EventArgs e)
         {
-
+            ADDTBINVLACHECK();
         }
         #endregion
 
